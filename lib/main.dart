5c9a1cb0c177185175a84e3308b9b9d15fcd0c18
rplinzner <37122411+rplinzner@users.noreply.github.com>
2019-04-29 20:55:36 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import './pages/home_page.dart';
+
+void main() => MainWindow();
+
+class MainWindow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {'/': (context) => HomePage()},
+      theme: ThemeData(
+          accentColor: Colors.orangeAccent,
+          primarySwatch: Colors.deepPurple,
+          brightness: Brightness.light),
+      title: 'Waste Monitor',
+    );
+  }
+}
