f9161767bf8b78720e5d8aa08e92f027cc0b4753
Rafał Plinzner <rplinzner@gmail.com>
2019-06-19 12:13:13 +0200

    [Add] histogram display

--- lib/pages/histogram_page.dart
+++ lib/pages/histogram_page.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import '../helpers/histogram_object.dart';
 
 class HistogramPage extends StatelessWidget {
   HistogramObject data = HistogramObject.noData();
   HistogramPage({this.data});
+  List<DayData> entries = List<DayData>();
+  final bool animate = true;
+
+  List<charts.Series<DayData, String>> generateEntries() {
+    entries.add(DayData("Mon", data.monday));
+    entries.add(DayData("Tue", data.tuesday));
+    entries.add(DayData("Wed", data.wednesday));
+    entries.add(DayData("Thu", data.thursday));
+    entries.add(DayData("Fri", data.friday));
+    entries.add(DayData("Sat", data.saturday));
+    entries.add(DayData("Sun", data.sunday));
+    return [
+      new charts.Series<DayData, String>(
+        id: 'Empties',
+        colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+        domainFn: (DayData day, _) => day.dayName,
+        measureFn: (DayData day, _) => day.count,
+        data: entries,
+      )
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +44,28 @@ class HistogramPage extends StatelessWidget {
                   style: TextStyle(
                       color: Colors.orange,
                       fontFamily: 'Montserrat',
-                      fontSize: 20.0),
-                ))
+                      fontSize: 23.0),
+                )),
+            Text(
+              'from previous month',
+              style: TextStyle(fontFamily: 'Montserrat', fontSize: 15),
+            ),
+            Expanded(
+              child: charts.BarChart(
+                generateEntries(),
+                animate: animate,
+              ),
+              flex: 3,
+            )
           ],
         ),
       ),
     );
   }
+}
+
+class DayData {
+  final String dayName;
+  final int count;
+  DayData(this.dayName, this.count);
 }
