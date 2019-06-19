22ab5a05124a869ffe69f922aceeaf505dd43f69
Rafał Plinzner <rplinzner@gmail.com>
2019-06-19 12:25:03 +0200

    [Add] lock orientation to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 import './pages/home_page.dart';
 import './pages/splash_screen.dart';
 import './pages/navigation.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MainWindow());
 
 class MainWindow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     return MaterialApp(
       initialRoute: '/',
       routes: {
